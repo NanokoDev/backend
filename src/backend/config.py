cc11fcc5a57d2827b98f4506250eb21873573152
zhaomaoniu <2667292003@qq.com>
2025-03-27 20:40:45 +1300

    feat: implement main

--- src/backend/config.py
+++ src/backend/config.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 from typing import Optional
-from dataclasses import dataclass
+from pydantic import BaseModel
 
 from backend.types.log import LogLevel
 
 
-@dataclass
-class Config:
+class Config(BaseModel):
     log_level: Optional[LogLevel] = LogLevel.INFO
+    host: Optional[str] = "127.0.0.1"
+    port: Optional[int] = 25324
     question_db_path: Optional[Path] = Path("data/question.db")
 
 
---
+++ src/main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from pathlib import Path
+from fastapi import FastAPI
+
+import backend.config as cfg
+from backend.api import router
+from backend.config import Config
+
+
+def load_config() -> Config:
+    config_path = Path("config.json")
+    if not config_path.exists():
+        config_path.write_text(Config().model_dump_json(indent=2), encoding="utf-8")
+    return Config.model_validate_json(config_path.read_text(encoding="utf-8"))
+
+
+def main(config: Config):
+    config.question_db_path.parent.mkdir(exist_ok=True)
+
+    app = FastAPI()
+    app.include_router(router, prefix="/api")
+
+    uvicorn.run(app, host=config.host, port=config.port)
+
+
+if __name__ == "__main__":
+    cfg.config = load_config()
+    main(cfg.config)

61f31d8197b560ca3c705d4f1ffcb38cfba0eeec
zhaomaoniu <2667292003@qq.com>
2025-04-01 18:43:36 +1300

    chore: change api models question.py to bank.py

---
+++ src/backend/api/models/bank.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+from backend.types.question import ConceptType, ProcessType
+
+
+class SubQuestion(BaseModel):
+    id: Optional[int] = None
+    description: str
+    answer: str
+    concept: ConceptType
+    process: ProcessType
+    keywords: Optional[List[str]] = None
+    image_id: Optional[int] = None
+
+
+class Question(BaseModel):
+    id: Optional[int] = None
+    source: str
+    is_audited: Optional[bool] = None
+    is_deleted: Optional[bool] = None
+    sub_questions: List[SubQuestion]

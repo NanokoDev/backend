2de436d8c2a8fb12b7e41cbe528af12cce13484d
zhaomaoniu <2667292003@qq.com>
2025-04-01 17:47:58 +1300

    fix: TypeError: exceptions must derive from BaseException

--- src/backend/exceptions/bank.py
+++ src/backend/exceptions/bank.py
@@ -1,4 +1,4 @@
-class BankDatabaseError:
+class BankDatabaseError(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
 
@@ -7,15 +7,15 @@ class BankDatabaseError:
 
 
 class SubQuestionIdInvalid(BankDatabaseError):
-    def __init__(self, sub_question_id: int):
+    def __init__(self, sub_question_id: int) -> None:
         super().__init__(f"Invalid sub_question_id {sub_question_id}")
 
 
 class QuestionIdInvalid(BankDatabaseError):
-    def __init__(self, question_id: int):
+    def __init__(self, question_id: int) -> None:
         super().__init__(f"Invalid question_id {question_id}")
 
 
 class ImageIdInvalid(BankDatabaseError):
-    def __init__(self, image_id: int):
+    def __init__(self, image_id: int) -> None:
         super().__init__(f"Invalid image_id {image_id}")

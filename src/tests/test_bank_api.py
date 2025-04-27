a6b680964c6dc7f6970260d313ada3f7d5c52f46
zhaomaoniu <2667292003@qq.com>
2025-04-27 16:17:54 +1200

    feat: output more prompt for testing

--- src/tests/test_bank_api.py
+++ src/tests/test_bank_api.py
@@ -43,7 +43,7 @@ def uploaded_image_hash(client, test_image_path):
     with open(test_image_path, "rb") as f:
         files = {"file": (test_image_path.name, f, "image/png")}
         response = client.post("/api/v1/bank/image/upload", files=files)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     return response.json()["hash"]
 
 
@@ -62,7 +62,7 @@ def image_id(client, uploaded_image_hash):
         "/api/v1/bank/image/add",
         json={"description": "A Test Image", "hash": uploaded_image_hash},
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     return response.json()["image_id"]
 
 
@@ -96,7 +96,7 @@ def question_id(client, image_id):
         ],
     }
     response = client.post("/api/v1/bank/question/add", json=question)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     return response.json()["question_id"]
 
 
@@ -110,7 +110,7 @@ def test_image_upload(client, test_image_path):
     with open(test_image_path, "rb") as f:
         files = {"file": (test_image_path.name, f, "image/png")}
         response = client.post("/api/v1/bank/image/upload", files=files)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json()["hash"]
     assert (
         cfg.config.image_store_path / "95d17bdeaca5d43432576b71043ef9f8.png"
@@ -119,11 +119,11 @@ def test_image_upload(client, test_image_path):
     with open(test_image_path, "rb") as f:
         files = {"file": (test_image_path.name, f, "image/gif")}
         response = client.post("/api/v1/bank/image/upload", files=files)
-    assert response.status_code == 400
+    assert response.status_code == 400, response.content
     assert "unsupported content_type" in response.json()["detail"].lower()
 
     response = client.post("/api/v1/bank/image/upload")
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
 
 
 def test_image_add(client, uploaded_image_hash):
@@ -137,18 +137,18 @@ def test_image_add(client, uploaded_image_hash):
         "/api/v1/bank/image/add",
         json={"description": "A Test Image", "hash": uploaded_image_hash},
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json()["image_id"]
 
     response = client.post(
         "/api/v1/bank/image/add",
         json={"description": "Unexist image", "hash": "1" * 32},
     )
-    assert response.status_code == 404
+    assert response.status_code == 404, response.content
     assert response.json()["detail"]
 
     response = client.post("/api/v1/bank/image/add")
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
 
 
 def test_image_get(client, image_id):
@@ -159,20 +159,20 @@ def test_image_get(client, image_id):
         image_id (int): the ID of the uploaded image
     """
     response = client.get("/api/v1/bank/image/get", params={"image_id": image_id})
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert len(response.content) == 515
 
     response = client.get("/api/v1/bank/image/get", params={"image_id": 100})
-    assert response.status_code == 404
+    assert response.status_code == 404, response.content
     assert response.json()["detail"]
 
     response = client.get("/api/v1/bank/image/get", params={"image_id": "100"})
-    assert response.status_code == 404
+    assert response.status_code == 404, response.content
 
     response = client.get(
         "/api/v1/bank/image/get", params={"image_id": "this is not an integer"}
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
 
 
 def test_question_add(client, image_id):
@@ -201,11 +201,11 @@ def test_question_add(client, image_id):
         ],
     }
     response = client.post("/api/v1/bank/question/add", json=question)
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json()["question_id"]
 
     response = client.post("/api/v1/bank/question/add")
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
 
 
 def test_question_get(client, question_id):
@@ -218,23 +218,23 @@ def test_question_get(client, question_id):
     response = client.get(
         "/api/v1/bank/question/get", params={"question_id": question_id}
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json() == []  # Question is not audited so no result
 
     response = client.get("/api/v1/bank/question/get", params={"question_id": 100})
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json() == []
 
     # TODO: Different authorisations with different responses
 
     response = client.get("/api/v1/bank/question/get", params={"question_id": "100"})
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json() == []
 
     response = client.get(
         "/api/v1/bank/question/get", params={"question_id": "this is not an integer"}
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
 
 
 def test_question_approve(client, question_id):
@@ -247,24 +247,24 @@ def test_question_approve(client, question_id):
     response = client.get(
         "/api/v1/bank/question/approve", params={"question_id": question_id}
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json()["msg"]
 
     response = client.get("/api/v1/bank/question/approve", params={"question_id": 100})
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
     assert "invalid" in response.json()["detail"].lower()
 
     response = client.get(
         "/api/v1/bank/question/approve", params={"question_id": "100"}
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
     assert "invalid" in response.json()["detail"].lower()
 
     response = client.get(
         "/api/v1/bank/question/approve",
         params={"question_id": "this is not an integer"},
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
 
 
 def test_question_delete(client, question_id):
@@ -277,22 +277,22 @@ def test_question_delete(client, question_id):
     response = client.delete(
         "/api/v1/bank/question/delete", params={"question_id": question_id}
     )
-    assert response.status_code == 200
+    assert response.status_code == 200, response.content
     assert response.json()["msg"]
 
     response = client.delete(
         "/api/v1/bank/question/delete", params={"question_id": 100}
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
     assert "invalid" in response.json()["detail"].lower()
 
     response = client.delete(
         "/api/v1/bank/question/delete", params={"question_id": "100"}
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
     assert "invalid" in response.json()["detail"].lower()
 
     response = client.delete(
         "/api/v1/bank/question/delete", params={"question_id": "this is not an integer"}
     )
-    assert response.status_code == 422
+    assert response.status_code == 422, response.content
